29dac9a9ffc0805957802e7f4154d49db87e9090
Sali_dk52@itstepru.onmicrosoft.com <Sali_dk52@itstepru.onmicrosoft.com>
2021-09-08 20:03:20 +0300

    old

--- DynamicArrays/main.cpp
+++ DynamicArrays/main.cpp
@@ -3,19 +3,6 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	/*int n;
-	cout << "¬ведите размер массива: "; cin >> n;
-	int* arr = new int[n] {};
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 100;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-	delete[] arr;*/
 	int n;
 	int Evencrl = 0, Oddcrl = 0;
 	cout << "¬ведите размер массива: "; cin >> n;
